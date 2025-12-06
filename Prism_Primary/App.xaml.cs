0dc112e3d0e15e0caa2f0dea50b5f6d64e331175
Junevy <junev.chung@gmail.com>
2025-12-06 23:43:07 +0800

    添加项目文件。

---
+++ Prism_Primary/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism_Primary.Views;
+
+namespace Prism_Primary
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
